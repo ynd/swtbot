f988150e76c245d08ade5301d03211995a0787ed
Yann N. Dauphin <yann@yann-laptop.(none)>
2009-06-15 09:59:08 -0400

    Added support for Spinners.

---
+++ org.eclipse.swtbot.swt.finder.test/src/org/eclipse/swtbot/swt/finder/widgets/SWTBotSpinnerTest.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2009 SWTBot Committers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Yann N. Dauphin - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swtbot.swt.finder.widgets;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.finders.AbstractSWTTestCase;
+import org.junit.Test;
+
+/**
+ * @author Yann N. Dauphin
+ * @version $Id$
+ */
+public class SWTBotSpinnerTest extends AbstractSWTTestCase {
+
+	SWTBot	bot	= new SWTBot();
+
+	@Test
+	public void findsSpinner() throws Exception {
+		assertNotNull(bot.spinner().widget);
+	}
+	
+	@Test
+	public void findsSpinnerInGroup() throws Exception {
+		assertNotNull(bot.spinnerInGroup("Spinner").widget);
+	}
+	
+	@Test
+	public void selectionWorks() throws Exception {
+		final int expected = 11;
+		bot.spinner().setSelection(expected);
+		assertEquals(expected, bot.spinner().getSelection());
+	}
+
+	public void setUp() throws Exception {
+		super.setUp();
+		bot.tabItem("Spinner").activate();
+	}
+
+}
